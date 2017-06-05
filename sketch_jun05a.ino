29cae5cf40be8c5531bbd23cc9bcbde46b455d00
simon90619 <simon1996524@gmail.com>
2017-06-05 19:38:57 +0800

    Add files via upload

---
+++ sketch_jun05a.ino
@@ -0,0 +1,23 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(11, 10, 5, 4, 3, 2);
+#include <Ultrasonic.h>
+#define TRIGGER_PIN 12
+#define ECHO_PIN 13
+Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+void setup() {
+Serial.begin(9600);
+ lcd.begin(20,2);
+}
+void loop(){
+float cmMsec, inMsec;
+long microsec = ultrasonic.timing();
+cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM); // 計算距離，單位: 公分
+inMsec = ultrasonic.convert(microsec, Ultrasonic::IN); // 計算距離，單位: 英吋
+Serial.print("MS: "); Serial.print(microsec);
+Serial.print(", CM: "); Serial.print(cmMsec);
+Serial.print(", IN: "); Serial.println(inMsec);
+lcd.print("CM:");
+lcd.print(cmMsec);
+delay(1000);
+lcd.clear();
+}
